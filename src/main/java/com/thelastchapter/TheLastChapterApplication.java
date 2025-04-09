7edf3ffe06f1eaa7ff2fb009618bcc86ce5fc172
RehanShaikh-eng <mdrehanshaikh341@gmail.com>
2025-04-10 03:13:08 +0530

    completed The Last Chapter

---
+++ src/main/java/com/thelastchapter/TheLastChapterApplication.java
@@ -0,0 +1,13 @@
+package com.thelastchapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheLastChapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheLastChapterApplication.class, args);
+	}
+
+}
